d8273dae1daa5cb15f714272f84d69c878a479b3
Lucía Vásquez <bvlucia34@gmail.com>
2023-08-30 23:08:19 -0600

    add paths in admin

--- src/app/features/admin/admin-routing.module.ts
+++ src/app/features/admin/admin-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('@features/admin').then(m => m.AdminLayoutModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -2,17 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { GraficalComponent } from './pages/grafical/grafical.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-
 
 @NgModule({
-  declarations: [
-    ProgressComponent,
-    GraficalComponent,
-    DashboardComponent
-  ],
+  declarations: [],
   imports: [
     CommonModule,
     AdminRoutingModule
