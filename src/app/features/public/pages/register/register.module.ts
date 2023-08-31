81099605ec47ffabb7a87f5f094603b0259cc246
Lucía Vásquez <bvlucia34@gmail.com>
2023-08-30 23:17:30 -0600

    add path in register

--- src/app/features/public/pages/register/register.module.ts
+++ src/app/features/public/pages/register/register.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RegisterRoutingModule } from './register-routing.module';
+import { RegisterComponent } from './register.component';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
 
 
 @NgModule({
   declarations: [
+    RegisterComponent,
     RegisterFormComponent
   ],
   imports: [
