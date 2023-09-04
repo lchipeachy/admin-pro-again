7de1d1f6f2ecb80e2b24314c0a93f18e42b10422
Lucía Vásquez <bvlucia34@gmail.com>
2023-09-04 13:38:11 -0600

    add btn

---
+++ src/app/shared/components/btn/btn.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-btn',
+  standalone: true,
+  imports: [ CommonModule ],
+  templateUrl: './btn.component.html',
+})
+export class BtnComponent {
+
+}
