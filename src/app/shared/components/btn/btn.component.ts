f15afdbc27062245aaecc820c8bd15648feb8291
Lucía Vásquez <bvlucia34@gmail.com>
2023-09-05 10:37:28 -0600

    btn colors

--- src/app/shared/components/btn/btn.component.ts
+++ src/app/shared/components/btn/btn.component.ts
@@ -1,5 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { COLOR } from '@core/dictionary';
+
+type color = typeof COLOR[keyof typeof COLOR];
 
 @Component({
   selector: 'shared-btn',
@@ -8,5 +12,23 @@ import { Component } from '@angular/core';
   templateUrl: './btn.component.html',
 })
 export class BtnComponent {
+  @Input()
+  public type: 'button' | 'submit' | 'reset' = 'button';
+  @Input()
+  public color: color = COLOR.PRIMARY;
 
+  get colors() {
+    return {
+      'btn-primary': this.color === 'primary',
+      'btn-secondary': this.color === 'secondary',
+      'btn-neutral': this.color === 'neutral',
+      'btn-accent': this.color === 'accent',
+      'btn-ghost': this.color === 'ghost',
+      'btn-success': this.color === 'success',
+      'btn-warning': this.color === 'warning',
+      'btn-error': this.color === 'error',
+      'btn-info': this.color === 'info',
+      'btn-link': this.color === 'link'
+    }
+  }
 }
