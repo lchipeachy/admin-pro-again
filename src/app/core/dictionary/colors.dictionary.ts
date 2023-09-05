382ad9b3ded3901e54665b2d4089f579aa2df1ea
Lucía Vásquez <bvlucia34@gmail.com>
2023-09-05 10:24:04 -0600

    add dictionary

---
+++ src/app/core/dictionary/colors.dictionary.ts
@@ -0,0 +1,12 @@
+export const COLOR = {
+    PRIMARY: 'primary',
+    SECONDARY: 'secondary', 
+    ACCENT: 'accent',
+    INFO: 'info',
+    SUCCESS: 'success',
+    NEUTRAL: 'neutral',
+    WARNING: 'warning',
+    ERROR: 'error',
+    GHOST: 'ghost',
+    LINK: 'link'
+} as const
