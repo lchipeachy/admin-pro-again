a3ab9a3e13c7bcdfcb92447e53638dabe5c14e92
Lucía Vásquez <bvlucia34@gmail.com>
2023-09-05 23:49:03 -0600

    add enum for dictionary

--- src/app/shared/components/btn/btn.component.ts
+++ src/app/shared/components/btn/btn.component.ts
@@ -1,9 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
-import { COLOR } from '@core/dictionary';
-
-type color = typeof COLOR[keyof typeof COLOR];
+import { Color } from '@core/enum';
 
 @Component({
   selector: 'shared-btn',
@@ -15,20 +13,17 @@ export class BtnComponent {
   @Input()
   public type: 'button' | 'submit' | 'reset' = 'button';
   @Input()
-  public color: color = COLOR.PRIMARY;
+  public color: Color = Color.PRIMARY;
+  @Input()
+  public outline = false;
+  @Input()
+  public btnDisabled = false;
 
   get colors() {
     return {
-      'btn-primary': this.color === 'primary',
-      'btn-secondary': this.color === 'secondary',
-      'btn-neutral': this.color === 'neutral',
-      'btn-accent': this.color === 'accent',
-      'btn-ghost': this.color === 'ghost',
-      'btn-success': this.color === 'success',
-      'btn-warning': this.color === 'warning',
-      'btn-error': this.color === 'error',
-      'btn-info': this.color === 'info',
-      'btn-link': this.color === 'link'
-    }
+      [`btn-${this.color}`]: true,
+      'btn-outline': this.outline,
+      'btn-disabled': this.btnDisabled
+    };
   }
 }
