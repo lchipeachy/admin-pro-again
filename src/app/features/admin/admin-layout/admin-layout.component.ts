6ad6053daa3fd6c55a192bca72f4fffb4a9f0317
Lucía Vásquez <bvlucia34@gmail.com>
2023-09-22 00:51:51 -0600

    add logout

--- src/app/features/admin/admin-layout/admin-layout.component.ts
+++ src/app/features/admin/admin-layout/admin-layout.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '@features/public';
 
 @Component({
   selector: 'admin-layout',
   templateUrl: './admin-layout.component.html',
 })
 export class AdminLayoutComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
+  onLogout():void {
+    this.authService.logout();
+    this.router.navigateByUrl('/');
+  }
 }
