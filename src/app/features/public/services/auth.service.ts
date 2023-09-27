df6f784b764342ef07131b98881607f9122460d9
Lucía Vásquez <bvlucia34@gmail.com>
2023-09-26 22:38:30 -0600

    register complete

--- src/app/features/public/services/auth.service.ts
+++ src/app/features/public/services/auth.service.ts
@@ -8,6 +8,8 @@ import {
     CheckTokenResponse,
     LoginForm,
     LoginResponse,
+    RegisterForm,
+    RegisterResponse,
     User,
 } from '../models';
 
@@ -15,24 +17,24 @@ import {
     providedIn: 'root',
 })
 export class AuthService {
-    // Signals privadas
+  // Signals privadas
     private _currentUser = signal<User | null>(null);
     private _authStatus = signal<AuthStatus>('checking');
 
-    //signal publicas al mundo exterior
+  //signal publicas al mundo exterior
     public CurrentUser = computed(() => this._currentUser());
     public authStatus = computed(() => this._authStatus());
 
-    // Inyecciones
+  // Inyecciones
     private apiService = inject(ApiService);
     private localStorageService = inject(LocalStorageService);
 
-    // Constructor
+  // Constructor
     constructor() {
         this.checkAuthStatus().subscribe();
     }
 
-    // Métodos
+  // Métodos
     login(loginForm: LoginForm): Observable<boolean> {
         return this.apiService.store<LoginResponse>('auth/login', loginForm).pipe(
         map(({ reply }) => this.setAuthentication(reply.user, reply.token)),
@@ -40,38 +42,46 @@ export class AuthService {
         );
     }
 
+    register(registerForm: RegisterForm) {
+        const { confirm_password, ...registerBody } = registerForm;
+
+        return this.apiService
+        .store<RegisterResponse>('auth/register', registerBody)
+        .pipe(catchError((error) => throwError(() => error.error.message)));
+    }
+
     checkAuthStatus(): Observable<boolean> {
         const token = this.localStorageService.getItem('token');
 
         if (!token) {
-        this.logout();
-        return of(false);
+            this.logout();
+            return of(false);
+        }
+
+        const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+        return this.apiService
+            .getAll<CheckTokenResponse>('auth/check-token', headers)
+            .pipe(
+                map(({ reply }) => this.setAuthentication(reply.user, reply.token)),
+                catchError(() => {
+                this._authStatus.set('noAuthenticated');
+                return of(false);
+                })
+            );
+        }
+
+    logout() {
+        this.localStorageService.removeItem('token');
+        this._currentUser.set(null);
+        this._authStatus.set('noAuthenticated');
     }
 
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    private setAuthentication(user: User, token: string): boolean {
+        this._currentUser.set(user);
+        this._authStatus.set('authenticated');
+        this.localStorageService.setItem('token', token);
 
-    return this.apiService
-        .getAll<CheckTokenResponse>('auth/check-token', headers)
-        .pipe(
-            map(({ reply }) => this.setAuthentication(reply.user, reply.token)),
-            catchError(() => {
-            this._authStatus.set('noAuthenticated');
-            return of(false);
-        })
-    );
-}
-
-logout() {
-    this.localStorageService.removeItem('token');
-    this._currentUser.set(null);
-    this._authStatus.set('noAuthenticated');
-}
-
-private setAuthentication(user: User, token: string): boolean {
-    this._currentUser.set(user);
-    this._authStatus.set('authenticated');
-    this.localStorageService.setItem('token', token);
-
-    return true;
+        return true;
     }
 }
